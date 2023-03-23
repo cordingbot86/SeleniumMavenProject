492efd128e67603787c0327d870aa4b73e3779c0
Gopika <123624154+cordingbot86@users.noreply.github.com>
2023-03-23 00:40:14 -0700

    day01

---
+++ src/test/java/day01/Project.java
@@ -0,0 +1,15 @@
+package day01;
+
+public class Project extends Framework{
+
+	public static void main(String[] args) {
+	
+		browser("edge");
+		appUrl("https://accounts.google.com/signup");
+		typeIn("//input[@name='firstName']");
+		typeIn("//input[@name='lastName']");
+		clickIn("//input[@type='checkbox']");
+		clearIn("//input[@name='firstName']");
+	}
+
+}
